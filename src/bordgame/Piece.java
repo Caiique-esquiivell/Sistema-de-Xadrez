e76b3499b5fd6a5c7b7e6e265eaea1a678f70716
Caique Esquivel <ecaique748@gmail.com>
2024-06-19 11:16:03 -0300

    new commit
    Starting to implement Board and Piece

---
+++ src/bordgame/Piece.java
@@ -0,0 +1,18 @@
+package bordgame;
+
+public class Piece {
+
+    protected  Position position;
+    private  Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+
+}
