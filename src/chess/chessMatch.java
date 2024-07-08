fb3cd0e04e9efd2193ba155d65b810cb4a582574
Caique Esquivel <ecaique748@gmail.com>
2024-07-08 19:43:39 -0300

    piece move count

--- src/chess/chessMatch.java
+++ src/chess/chessMatch.java
@@ -83,7 +83,8 @@ public class chessMatch {
     }
 
     private Piece makeMove(Position source, Position target){
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.decreaseMoveCount();
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p,target);
 
@@ -96,7 +97,8 @@ public class chessMatch {
     }
 
     private  void undoMove(Position source, Position target, Piece capturedPiece){
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece)board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
 
         if (capturedPiece != null){
