096dab0e43d88219858beb797b5fac2025840117
Caique Esquivel <ecaique748@gmail.com>
2024-07-09 18:39:53 -0300

    special move - castling

--- src/chess/pices/King.java
+++ src/chess/pices/King.java
@@ -4,11 +4,15 @@ import bordgame.Board;
 import bordgame.Position;
 import chess.ChessPiece;
 import chess.Color;
+import chess.chessMatch
 
 public class King extends ChessPiece {
 
-    public King(Board board, Color color) {
+    private  chessMatch chessMatch;
+
+    public King(Board board, Color color, chessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -21,6 +25,10 @@ public class King extends ChessPiece {
         return  p == null || p.getColor() != getColor();
     }
 
+    private  boolean testRookCastling(Position position){
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p instanceof  Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
+    }
 
     @Override
     public boolean[][] posibleMoves() {
@@ -59,5 +67,27 @@ public class King extends ChessPiece {
         if (getBoard().positionExists(p) && canMove(p)){
             mat[p.getRow()][p.getColumn()] = true;
         }
-        return mat;}
+
+        if (getMoveCount() == 0 && !chessMatch.getCheck()){
+            Position post1 = new Position(position.getRow(),position.getColumn()+3);
+            if (testRookCastling(post1)){
+                Position p1 = new Position(position.getRow(),position.getColumn()+1);
+                Position p2 = new Position(position.getRow(),position.getColumn()+2);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2)==null){
+                    mat[position.getRow()][position.getColumn()+2] = true;
+                }
+            }
+            Position post2 = new Position(position.getRow(),position.getColumn()-4);
+            if (testRookCastling(post2)){
+                Position p1 = new Position(position.getRow(),position.getColumn()-1);
+                Position p2 = new Position(position.getRow(),position.getColumn()-2);
+                Position p3 = new Position(position.getRow(),position.getColumn()-2);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2)==null && getBoard().piece(p3)==null){
+                    mat[position.getRow()][position.getColumn()-2] = true;
+                }
+            }
+        }
+
+        return mat;
+    }
 }
--- src/chess/pices/Pawn.java
+++ src/chess/pices/Pawn.java
@@ -7,6 +7,8 @@ import chess.Color;
 
 public class Pawn extends ChessPiece {
 
+
+
     public Pawn(Board board, Color color){
         super(board,color);
     }
@@ -16,6 +18,7 @@ public class Pawn extends ChessPiece {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
         Position p = new Position(0,0);
+        
         if (getColor() == Color.WHITE){
             p.setValues(position.getRow()-1,position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
